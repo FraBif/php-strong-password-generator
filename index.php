14f33ba15e3d63c6d457311aaff18eae13e6f6a2
FraBif <francescobif@hotmail.it>
2023-07-04 18:51:05 +0200

    Created html form

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Generatore di Password</title>
+</head>
+<body>
+    <h1>
+        Generatore Casuale di Password
+    </h1>
+    <form action="" method="GET">
+        <label for="passwordLength">Lunghezza della tua Password:</label>
+        <input type="number" name="passwordLength" id="passwordLength">
+        <button type="submit">Genera La Tua Password</button>
+    </form>
+</body>
+</html>
