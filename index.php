84529885aceccddfbac5f01b2f52720f098566cd
FraBif <francescobif@hotmail.it>
2023-07-07 15:03:15 +0200

    modified index and adjusted functions

--- index.php
+++ index.php
@@ -9,54 +9,54 @@
     <h1>
         Generatore Casuale di Password
     </h1>
-    <form action="" method="GET">
+    <form action="./index.php" method="GET">
         <label for="passwordLength">Lunghezza della tua Password:</label>
-        <input type="number" name="passwordLength" id="passwordLength">
+        <input type="number" name="number" id="number">
         <button type="submit">Genera La Tua Password</button>
     </form>
+
+    <div>
+        <?php
+        echo generateRandomPassword($length)
+        ?>
+    </div>
 
     <?php 
         $passwordLength = null;
 
-if (!empty($_GET['typeNumber'])){
-    $passwordLength = checkData($_GET['typeNumber']);
-}
+        if (!empty($_GET['number'])){
+            $passwordLength = checkData($_GET['number']);
+        }
 
-function checkData ($data){
-    if (!is_numeric($data)){
-        return false;
-    }
-    return $data;
-}
+        function checkData ($data){
+            if (!is_numeric($data)){
+                return false;
+            }
+            return $data;
+        }
 
-function generateRandomPassword ($length) {
-    $password = '';
-    $lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-    $upperCase = 'ABCDEFGHJKLMNPQRSTUVWXYZ';
-    $numbers =  '0123456789' ;
-    $specialChar = '!@#$%^&*()_+-={}[]|:;"<>,.?/';
+        function generateRandomPassword ($length) {
+            $password = '';
+            $lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+            $upperCase = 'ABCDEFGHJKLMNPQRSTUVWXYZ';
+            $numbers =  '0123456789' ;
+            $specialChar = '!@#$%^&*()_+-={}[]|:;"<>,.?/';
 
-    $allChar = [ $lowerCase , $upperCase , $numbers , $specialChar ];
+            $allChar = [ $lowerCase , $upperCase , $numbers , $specialChar ];
 
 
-    
-    foreach ($allChar as $char){
-        $password .= $char[array_rand(str_split($char))];
-    }
+            
+            foreach ($allChar as $char){
+                $password .= $char[array_rand(str_split($char))];
+            }
 
-    while (strlen($password) < $length){
-        $random = $allChar[array_rand($allChar)];
-        $password .= $random[array_rand(str_split($random))];
-    }
+            while (strlen($password) < $length){
+                $random = $allChar[array_rand($allChar)];
+                $password .= $random[array_rand(str_split($random))];
+            }
 
-    return $password;
-}
+            return $password;
+        }
     ?>
-
-    <p>
-        <?php
-        echo $password
-        ?>
-    </p>
 </body>
 </html>
